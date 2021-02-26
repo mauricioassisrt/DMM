c59ac842f7c1513c70173f0b2e2588d4596df430
Mauricio de Assis <mauriciox8mm@hotmail.com>
2021-02-26 12:00:56 -0300

    aula 18 completada

--- financeirofacil/src/app/recebidos/recebidos.page.ts
+++ financeirofacil/src/app/recebidos/recebidos.page.ts
@@ -1,3 +1,6 @@
+
+import { Lancamentos } from '../model/lancamentos';
+import { LancamentosService } from '../services/lancamentos.service';
 import { FormComponent } from './form/form.component';
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
@@ -8,16 +11,33 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./recebidos.page.scss'],
 })
 export class RecebidosPage implements OnInit {
+  listaLancamentos = [];
 
+  constructor(public modalController: ModalController, public lancamentoService:LancamentosService) { }
 
-  constructor(public modalController: ModalController) {}
+  ngOnInit() {
+
+    let Lancamentos = this.lancamentoService.buscarTodos();
+    Lancamentos.snapshotChanges().subscribe(res => {
+      this.listaLancamentos = [];
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+        // let mesSalvo = new Date(a['dataLacamento']).getMonth();
+        // let anoSalvo = new Date(a['dataLacamento']).getFullYear();
+        //  if (mesSelecionado == mesSalvo && anoSelecionado == anoSalvo && a['tipo']=='recebido') {
+        if (a['tipo'] == 'recebido') {
+          a['key'] = item.key;
+          this.listaLancamentos.push(a as Lancamentos);
+        }
+      })
+    })
+
+  }
+
   async chamarFormulario() {
     const modal = await this.modalController.create({
       component: FormComponent
     });
     return await modal.present();
   }
-  ngOnInit() {
-  }
-
 }
